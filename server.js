edadb2539fd10d7f0b8c587c08100c411fe6b440
JasonRaza <jason_raza@hotmail.com>
2024-03-04 08:18:52 -0500

    Co-authored-by: Hocineben13 <Hocineben13@users.noreply.github.com> Co-authored-by: Benbara, Hocine <6211328@bdeb.qc.ca> Co-authored-by: 1966934 <1966934@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/database');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const connectDB = require('./config/database');
+const crypto = require('crypto');
 const utilisateurModel = require('./model/utilisateurModel');
+const Trajet = require('./model/trajetModel');
 const app = express();
 const path = require('path');
 const PORT = 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:true}));
+
+const generateSecret = () => {
+    return crypto.randomBytes(32).toString('hex');
+};
+app.use(session({
+    secret: generateSecret(),
+    resave: false,
+    saveUninitialized: false
+}));
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -76,10 +89,10 @@ app.post('/inscription', async (req,res) =>{
     }
 })
 
-app.post('/connexion', async (req, res)=> {
+app.post('/profil', async (req, res)=> {
     try{
         const { email, password} = req.body;
-
+        
         const utilisateur = await utilisateurModel.findOne({email});
 
         if(!utilisateur){
@@ -90,22 +103,95 @@ app.post('/connexion', async (req, res)=> {
         if(!isMatch){
             return res.status(401).send('Mot de passe incorrect');
         }
-        res.render('profil');
+
+        const nom = utilisateur.nom;
+        const prenom = utilisateur.prenom;
+
+        //Capitalise la premiere lettre du nom et du prenom
+        const nomCapitalise = nom.charAt(0).toUpperCase() + nom.slice(1).toLowerCase();
+        const prenomCapitalise = prenom.charAt(0).toUpperCase() + prenom.slice(1).toLowerCase();
+
+        let trajets= await Trajet.find(); 
+        //console.log(trajets); J'ai mis en commentaire sinon ca rempli trop le terminal, enlever commentaire pour tester
+
+
+        res.render('profil', { nom: nomCapitalise, prenom: prenomCapitalise, trajets });        
+        console.log('Page profil utilisateur ouvert.');
+
     }catch(err){
         console.error(err);
+        res.status(500).send('Erreur lors de la récupération des trajets');
     }
 })
 
-//Page de profil utilisateur
-app.get('/profil', (req,res) => {
-    try {
-        res.render('profil', {nom, prenom});
-        console.log("Page profil utilisateur ouvert")
+app.post('/creer-trajet', async (req, res)=> {
+    try{
+        const { depart, arrivee, dateDepart, heureDepart, voiture } = req.body;
+        
+        const userId = req.session.userId;
+        
+        const nouveauTrajet = new Trajet({
+            pointDepart: depart,
+            pointDestination: arrivee,
+            dateDepart: dateDepart,
+            heureDepart: heureDepart,
+            voiture: voiture,
+            con
+ :ruetcu
+        })
+
+        await nouveauTrajet.save();
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Erreur lors de la création du trajet');
     }
-    catch(error){
-        console.log('Erreur ouverture page profil utilisateur')
+})
+
+app.post('/rechercher-trajet', async (req, res) => {
+    try{
+        const{ pointDepart, pointDestination } = req.body;
+        let critereRecherche={};
+        
+        if (pointDepart && pointDestination){
+            critereRecherche = {pointDepart: pointDepart, pointDestination: pointDestination};
+        } else if(pointDepart){
+            critereRecherche = { pointDepart: pointDepart};
+        }else if(pointDestination){
+            critereRecherche = { pointDestination: pointDestination};
+        }
+
+        const trajetsTrouves = await Trajet.find(critereRecherche);
+
+        if(trajetsTrouves.length > 0){
+            res.render('resultat-recherche', { trajets: trajetsTrouves, message: 'Trajet trouvé'});
+            console.log('Point de départ:', pointDepart);
+            console.log('Point de destination:', pointDestination);
+            console.log('Critères de recherche:', critereRecherche);
+            
+        } else{
+            res.render('resultat-recherche', { trajets : [], message: 'Trajet non trouvé'});
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Erreur lors de la recherche de trajets');
     }
 });
+
+
+app.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                console.error('Erreur lors de la déconnexion :', err);
+                return res.status(500).send('Erreur lors de la déconnexion');
+            }
+            res.redirect('/');
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
 
 //Message dans le terminal qui indique le port lorsque le serveur est lancé
 app.listen(PORT, () => {
